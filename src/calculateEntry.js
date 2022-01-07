a1e0c3dbcafe8f2e9c543828bb11d82f38cbad7d
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-07 18:12:21 -0300

    6. calculateEntry OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
+function numOfEntrants(entrants) {
+  const visitors = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) visitors.child += 1;
+    else if (element.age >= 18 && element.age < 50) visitors.adult += 1;
+    else if (element.age >= 50) visitors.senior += 1;
+  });
+  return visitors;
+}
+
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  return numOfEntrants(entrants);
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!countEntrants(entrants)) {
+    return countEntrants(entrants);
+  }
+  const visitors = countEntrants(entrants);
+  const pay = data.prices.child * visitors.child
+    + data.prices.adult * visitors.adult
+    + data.prices.senior * visitors.senior;
+  return pay;
 }
 
 module.exports = { calculateEntry, countEntrants };
