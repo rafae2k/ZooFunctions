1477851b8585f89011bba7f9707f7409217e0ca9
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-07 18:06:56 -0300

    3. getEmployeeByName OK

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(name) {
   // seu código aqui
+  const employee = {};
+
+  if (name === undefined) return employee;
+
+  const findByName = (employeeName, index) => {
+    if (name === employeeName.firstName || name === employeeName.lastName) {
+      Object.assign(employee, data.employees[index]);
+    }
+  };
+
+  data.employees.filter(findByName);
+  return employee;
 }
 
 module.exports = getEmployeeByName;
