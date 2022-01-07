383772c719259c01bbe1d1f000f7603f9af645cd
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-07 18:09:36 -0300

    4. getRelatedEmployees OK

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function isManager(code) {
+  return data.employees.some((employees) =>
+    employees.managers.find((ids) => ids === code));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const responsable = [];
+    data.employees.forEach((employee) => {
+      if (employee.managers.includes(managerId)) {
+        responsable.push(`${employee.firstName} ${employee.lastName}`);
+      }
+    });
+    return responsable;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
