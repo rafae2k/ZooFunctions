22941ba19a58ffeaaa53b71f7132928da05ebc8c
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-07 18:01:07 -0300

    2. getAnimalsOlderThan OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,16 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+
+  const specieObj = data.species.filter(
+    (element, indexSpecies) => element.name === animal,
+  );
+
+  const ageGreaterThan = specieObj[0].residents.every(
+    (resident) => resident.age > age,
+  );
+
+  return ageGreaterThan;
 }
 
 module.exports = getAnimalsOlderThan;
