77f545ff2c5829df3c487765dd346f4acde73b30
rafae2k <53544360+rafae2k@users.noreply.github.com>
2022-01-07 23:13:15 -0300

    9. getOldestFromFirstSpecies OK

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(...ids) {
+  let older;
+  let animalByResponsableId;
+  ids.forEach((id) => {
+    animalByResponsableId = employees.find((employee) => employee.id === id);
+
+    const animal = species.find(
+      (specie) => animalByResponsableId.responsibleFor[0] === specie.id,
+    );
+    older = animal.residents.reduce((acc, resident) => (acc.age > resident.age ? acc : resident));
+  });
+  return Object.values(older);
 }
 
 module.exports = getOldestFromFirstSpecies;
