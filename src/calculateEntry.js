99e299cedb248851c7b1960e42b0f26f5edd81a9
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-01-27 12:13:20 -0300

    Update calculateEntry.js

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function numOfEntrants(entrants) {
   const visitors = { child: 0, adult: 0, senior: 0 };
+
   entrants.forEach((element) => {
     if (element.age < 18) visitors.child += 1;
     else if (element.age >= 18 && element.age < 50) visitors.adult += 1;
@@ -22,9 +23,10 @@ function calculateEntry(entrants) {
     return countEntrants(entrants);
   }
   const visitors = countEntrants(entrants);
-  const pay = data.prices.child * visitors.child
-    + data.prices.adult * visitors.adult
-    + data.prices.senior * visitors.senior;
+  const pay =
+    data.prices.child * visitors.child +
+    data.prices.adult * visitors.adult +
+    data.prices.senior * visitors.senior;
   return pay;
 }
 
